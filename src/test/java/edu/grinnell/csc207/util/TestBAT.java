ece805b48953e4e1c8265db0ba4c478507a84bfa
Samuel A. Rebelsky <rebelsky@grinnell.edu>
2024-11-22 16:56:13 -0600

    Add blank test files.

---
+++ src/test/java/edu/grinnell/csc207/util/TestBAT.java
@@ -0,0 +1,32 @@
+package edu.grinnell.csc207.util;
+
+import static org.junit.jupiter.api.Assertions.assertArrayEquals;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertThrows;
+
+import org.junit.jupiter.api.Test;
+
+
+/**
+ * Some simple tests of our BrailleAsciiTables class.
+ *
+ * @author Samuel A. Rebelsky
+ */
+public class TestBAT {
+  // +---------------+-----------------------------------------------
+  // | Static fields |
+  // +---------------+
+
+  // +---------+-----------------------------------------------------
+  // | Helpers |
+  // +---------+
+
+  // +-------+-------------------------------------------------------
+  // | Setup |
+  // +-------+
+
+  // +-------+-------------------------------------------------------
+  // | Tests |
+  // +-------+
+
+} // class TestBAT
---
+++ src/test/java/edu/grinnell/csc207/util/TestBitTree.java
@@ -0,0 +1,32 @@
+package edu.grinnell.csc207.util;
+
+import static org.junit.jupiter.api.Assertions.assertArrayEquals;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertThrows;
+
+import org.junit.jupiter.api.Test;
+
+
+/**
+ * Some simple tests of our BitTree class.
+ *
+ * @author Samuel A. Rebelsky
+ */
+public class TestBitTree {
+  // +---------------+-----------------------------------------------
+  // | Static fields |
+  // +---------------+
+
+  // +---------+-----------------------------------------------------
+  // | Helpers |
+  // +---------+
+
+  // +-------+-------------------------------------------------------
+  // | Setup |
+  // +-------+
+
+  // +-------+-------------------------------------------------------
+  // | Tests |
+  // +-------+
+
+} // class TestBitTree
